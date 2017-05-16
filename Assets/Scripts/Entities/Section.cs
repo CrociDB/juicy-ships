b895a3e21ae92d861c47fcce25c559f8ffbb9fb6
Bruno Croci <crocidb@gmail.com>
2017-05-16 00:50:38 -0400

    added basic section movement

---
+++ Assets/Scripts/Entities/Section.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Section : MonoBehaviour
+{
+    public float m_Speed = 1f;
+
+	void Update ()
+    {
+        transform.position += transform.forward * -m_Speed;	
+	}
+}
