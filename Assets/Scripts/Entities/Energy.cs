69dfe54d8dc9a04dbd8e22dc996580a797149a54
Bruno Croci <crocidb@gmail.com>
2017-07-11 01:16:41 -0400

    improved game feel for a build

--- Assets/Scripts/Entities/Energy.cs
+++ Assets/Scripts/Entities/Energy.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +8,6 @@ namespace Entities
 {
     public class Energy : Entity
     {
-
+        
     }
 }
