5a9a6f06deb8bd8a29fc463451fc62261e874641
Bruno Croci <crocidb@gmail.com>
2017-07-10 21:57:31 -0400

    created energy entity, still not working

---
+++ Assets/Scripts/Entities/EntitiesMovement.cs
@@ -0,0 +1,24 @@
+﻿using SimpleStateMachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    public class EntitiesMovement : MonoBehaviour
+    {
+        public float m_SpeedRotation = .1f;
+        public float m_SpeedMovement = 5;
+        public float m_AmountMovement = .1f;
+
+        private void Update()
+        {
+            transform.localPosition = new Vector3(
+                0f,
+                Mathf.Cos(Time.time * m_SpeedMovement) * m_AmountMovement,
+                0f);
+
+            transform.Rotate(transform.up, (m_SpeedRotation * Time.deltaTime));
+        }
+    }
+}
